4b4b8e740688609d41b464d31199ee38b5aed745
Maksym Skrypnyk <moxskr@gmail.com>
2024-09-26 16:32:58 +0300

    make some refactoring, change prettier config, add controller class for csv import component, add csv import component

--- force-app/main/default/lwc/fileUploadZone/__tests__/fileUploadZone.test.js
+++ force-app/main/default/lwc/fileUploadZone/__tests__/fileUploadZone.test.js
@@ -1,153 +1,160 @@
-import { createElement } from "@lwc/engine-dom";
-import FileUploadZone from "c/fileUploadZone";
+import { createElement } from '@lwc/engine-dom';
+import FileUploadZone from 'c/fileUploadZone';
 
-describe("c-file-upload-zone", () => {
-  afterEach(() => {
-    while (document.body.firstChild) {
-      document.body.removeChild(document.body.firstChild);
-      jest.clearAllMocks();
-    }
-  });
+describe('c-file-upload-zone', () => {
+	afterEach(() => {
+		while (document.body.firstChild) {
+			document.body.removeChild(document.body.firstChild);
+			jest.clearAllMocks();
+		}
+	});
 
-  it("test drag and drop", async () => {
-    const element = createElement("c-file-upload-zone", {
-      is: FileUploadZone
-    });
+	it('test drag and drop', async () => {
+		const element = createElement('c-file-upload-zone', {
+			is: FileUploadZone
+		});
 
-    element.maxSize = 1000;
-    element.formats = ["text/csv"]
+		element.maxSize = 1000;
+		element.formats = ['text/csv'];
 
-    document.body.appendChild(element);
+		document.body.appendChild(element);
 
-    const handler = jest.fn();
+		const handler = jest.fn();
 
-    element.addEventListener("fileupload", handler);
+		element.addEventListener('fileupload', handler);
 
-    const dragZone = element.shadowRoot.querySelector("div[data-id=dragZone");
+		const dragZone = element.shadowRoot.querySelector(
+			'div[data-id=dragZone'
+		);
 
-    const testFile = createTestFile();
+		const testFile = createTestFile();
 
-    const dropEvent = new CustomEvent("drop");
+		const dropEvent = new CustomEvent('drop');
 
-    dropEvent.dataTransfer = {
-      files: [testFile]
-    };
+		dropEvent.dataTransfer = {
+			files: [testFile]
+		};
 
-    dragZone.dispatchEvent(dropEvent);
+		dragZone.dispatchEvent(dropEvent);
 
-    await flushPromises();
+		await flushPromises();
 
-    expect(handler).toHaveBeenCalledWith(
-      expect.objectContaining({
-        detail: expect.objectContaining({
-          file: expect.objectContaining({
-            name: testFile.name,
-            type: testFile.type,
-            size: testFile.size
-          })
-        })
-      })
-    );
-  });
+		expect(handler).toHaveBeenCalledWith(
+			expect.objectContaining({
+				detail: expect.objectContaining({
+					file: expect.objectContaining({
+						name: testFile.name,
+						type: testFile.type,
+						size: testFile.size
+					})
+				})
+			})
+		);
+	});
 
-  it("test adding file using input", async () => {
-    const element = createElement("c-file-upload-zone", {
-      is: FileUploadZone
-    });
+	it('test adding file using input', async () => {
+		const element = createElement('c-file-upload-zone', {
+			is: FileUploadZone
+		});
 
-    element.maxSize = 1000;
-    element.formats = ["text/csv"]
+		element.maxSize = 1000;
+		element.formats = ['text/csv'];
 
-    document.body.appendChild(element);
+		document.body.appendChild(element);
 
-    const handler = jest.fn();
+		const handler = jest.fn();
 
-    element.addEventListener("fileupload", handler);
+		element.addEventListener('fileupload', handler);
 
-    const input = element.shadowRoot.querySelector("lightning-input");
+		const input = element.shadowRoot.querySelector('lightning-input');
 
-    const testFile = createTestFile();
+		const testFile = createTestFile();
 
-    const inputEvent = new CustomEvent("change");
+		const inputEvent = new CustomEvent('change');
 
-    Object.defineProperty(inputEvent, "target", {
-      value: {
-        files: [testFile]
-      }
-    });
+		Object.defineProperty(inputEvent, 'target', {
+			value: {
+				files: [testFile]
+			}
+		});
 
-    input.dispatchEvent(inputEvent);
+		input.dispatchEvent(inputEvent);
 
-    await flushPromises();
+		await flushPromises();
 
-    expect(handler).toHaveBeenCalledWith(
-      expect.objectContaining({
-        detail: expect.objectContaining({
-          file: expect.objectContaining({
-            name: testFile.name,
-            type: testFile.type,
-            size: testFile.size
-          })
-        })
-      })
-    );
-  });
+		expect(handler).toHaveBeenCalledWith(
+			expect.objectContaining({
+				detail: expect.objectContaining({
+					file: expect.objectContaining({
+						name: testFile.name,
+						type: testFile.type,
+						size: testFile.size
+					})
+				})
+			})
+		);
+	});
 
-  it("test validation", async () => {
-    const element = createElement("c-file-upload-zone", {
-      is: FileUploadZone
-    });
+	it('test validation', async () => {
+		const element = createElement('c-file-upload-zone', {
+			is: FileUploadZone
+		});
 
-    const largePlainFile = createTestFile("test123123qweqweqwe", "text/plain");
-    const smallCsvFile = createTestFile("test", "text/csv");
+		const largePlainFile = createTestFile(
+			'test123123qweqweqwe',
+			'text/plain'
+		);
+		const smallCsvFile = createTestFile('test', 'text/csv');
 
-    element.maxSize = 10;
-    element.formats = ["text/plain"];
+		element.maxSize = 10;
+		element.formats = ['text/plain'];
 
-    document.body.appendChild(element);
+		document.body.appendChild(element);
 
-    const fileUploadHandler = jest.fn();
-    const showToastHandler = jest.fn();
+		const fileUploadHandler = jest.fn();
+		const showToastHandler = jest.fn();
 
-    element.addEventListener("fileupload", fileUploadHandler);
-    element.addEventListener('lightning__showtoast', showToastHandler);
+		element.addEventListener('fileupload', fileUploadHandler);
+		element.addEventListener('lightning__showtoast', showToastHandler);
 
-    const dragZone = element.shadowRoot.querySelector("div[data-id=dragZone");
+		const dragZone = element.shadowRoot.querySelector(
+			'div[data-id=dragZone'
+		);
 
-    const dropEventLarge = new CustomEvent("drop");
+		const dropEventLarge = new CustomEvent('drop');
 
-    dropEventLarge.dataTransfer = {
-      files: [largePlainFile]
-    };
+		dropEventLarge.dataTransfer = {
+			files: [largePlainFile]
+		};
 
-    dragZone.dispatchEvent(dropEventLarge);
+		dragZone.dispatchEvent(dropEventLarge);
 
-    await flushPromises();
+		await flushPromises();
 
-    expect(fileUploadHandler).not.toHaveBeenCalled();
-    expect(showToastHandler).toHaveBeenCalledTimes(1);
+		expect(fileUploadHandler).not.toHaveBeenCalled();
+		expect(showToastHandler).toHaveBeenCalledTimes(1);
 
-    const dropEventSmall = new CustomEvent("drop");
+		const dropEventSmall = new CustomEvent('drop');
 
-    dropEventSmall.dataTransfer = {
-      files: [smallCsvFile]
-    };
+		dropEventSmall.dataTransfer = {
+			files: [smallCsvFile]
+		};
 
-    dragZone.dispatchEvent(dropEventSmall);
+		dragZone.dispatchEvent(dropEventSmall);
 
-    await flushPromises();
+		await flushPromises();
 
-    expect(fileUploadHandler).not.toHaveBeenCalled();
-    expect(showToastHandler).toHaveBeenCalledTimes(2);
-  });
+		expect(fileUploadHandler).not.toHaveBeenCalled();
+		expect(showToastHandler).toHaveBeenCalledTimes(2);
+	});
 });
 
 function flushPromises() {
-  return Promise.resolve(process.nextTick);
+	return Promise.resolve(process.nextTick);
 }
 
-function createTestFile(content = "test", format = "text/csv") {
-  const file = new File([content], "test.csv", { type: format });
+function createTestFile(content = 'test', format = 'text/csv') {
+	const file = new File([content], 'test.csv', { type: format });
 
-  return file;
+	return file;
 }
--- force-app/main/default/lwc/fileUploadZone/fileUploadZone.js
+++ force-app/main/default/lwc/fileUploadZone/fileUploadZone.js
@@ -1,90 +1,88 @@
-import { api, LightningElement } from "lwc";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { api, LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class FileUploadZone extends LightningElement {
-  @api
-  formats = ["text/plain"];
+	@api
+	formats = ['text/plain'];
 
-  @api
-  maxSize = 3 * 1024 * 1024;
+	@api
+	maxSize = 3 * 1024 * 1024;
 
-  handleFileInputChange(event) {
-    console.log(event);
+	handleFileInputChange(event) {
+		const [file] = event.target.files;
 
-    const [file] = event.target.files;
+		if (file) {
+			const isValid = this.validateFile(file);
 
-    if (file) {
-      const isValid = this.validateFile(file);
+			if (isValid) {
+				this.fireFileUploadEvent(file);
+			}
+		}
+	}
 
-      if (isValid) {
-        this.fireFileUploadEvent(file);
-      }
-    }
-  }
+	handleFileDrag(event) {
+		event.preventDefault();
+	}
 
-  handleFileDrag(event) {
-    event.preventDefault();
-  }
+	handleFileDrop(event) {
+		event.preventDefault();
 
-  handleFileDrop(event) {
-    event.preventDefault();
+		const { dataTransfer } = event;
 
-    const { dataTransfer } = event;
+		if (dataTransfer) {
+			const [file] = dataTransfer.files;
 
-    if (dataTransfer) {
-      const [file] = dataTransfer.files;
+			const isValid = this.validateFile(file);
 
-      const isValid = this.validateFile(file);
+			if (isValid) {
+				this.fireFileUploadEvent(file);
+			}
+		}
+	}
 
-      if (isValid) {
-        this.fireFileUploadEvent(file);
-      }
-    }
-  }
+	validateFile(file) {
+		return this.validateSize(file) && this.validateFormat(file);
+	}
 
-  validateFile(file) {
-    return this.validateSize(file) && this.validateFormat(file);
-  }
+	validateSize(file) {
+		const isValid = file.size <= this.maxSize;
 
-  validateSize(file) {
-    const isValid = file.size <= this.maxSize;
+		if (!isValid) {
+			const toastEvent = new ShowToastEvent({
+				title: 'File too large',
+				message: `File size must be less than ${this.maxSize} bytes`,
+				variant: 'error'
+			});
 
-    if (!isValid) {
-      const toastEvent = new ShowToastEvent({
-        title: "File too large",
-        message: `File size must be less than ${this.maxSize} bytes`,
-        variant: "error"
-      });
+			this.dispatchEvent(toastEvent);
+		}
 
-      this.dispatchEvent(toastEvent);
-    }
+		return isValid;
+	}
 
-    return isValid;
-  }
+	validateFormat(file) {
+		const isValid = this.formats.includes(file.type);
 
-  validateFormat(file) {
-    const isValid = this.formats.includes(file.type);
+		if (!isValid) {
+			const toastEvent = new ShowToastEvent({
+				title: 'File has wrong format type',
+				message: `Only ${this.formats.join(',')} are allowed`,
+				variant: 'error'
+			});
 
-    if (!isValid) {
-      const toastEvent = new ShowToastEvent({
-        title: "File has wrong format type",
-        message: `Only ${this.formats.join(",")} are allowed`,
-        variant: "error"
-      });
+			this.dispatchEvent(toastEvent);
+		}
 
-      this.dispatchEvent(toastEvent);
-    }
+		return isValid;
+	}
 
-    return isValid;
-  }
+	fireFileUploadEvent(file) {
+		const event = new CustomEvent('fileupload', {
+			detail: {
+				file
+			}
+		});
 
-  fireFileUploadEvent(file) {
-    const event = new CustomEvent("fileupload", {
-      detail: {
-        file
-      }
-    });
-
-    this.dispatchEvent(event);
-  }
+		this.dispatchEvent(event);
+	}
 }
